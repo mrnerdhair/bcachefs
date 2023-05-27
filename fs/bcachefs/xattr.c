7cb00ab6e257146ff09bdb1a223378786f6bb90c
Kent Overstreet <kent.overstreet@linux.dev>
2023-05-27 19:59:59 -0400

    bcachefs: Convert -ENOENT to private error codes
    As with previous conversions, replace -ENOENT uses with more informative
    private error codes.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/xattr.c
+++ fs/bcachefs/xattr.c
@@ -163,7 +163,7 @@ static int bch2_xattr_get_trans(struct btree_trans *trans, struct bch_inode_info
 err2:
 	bch2_trans_iter_exit(trans, &iter);
 err1:
-	return ret == -ENOENT ? -ENODATA : ret;
+	return bch2_err_matches(ret, ENOENT) ? -ENODATA : ret;
 }
 
 int bch2_xattr_get(struct bch_fs *c, struct bch_inode_info *inode,
@@ -229,7 +229,7 @@ int bch2_xattr_set(struct btree_trans *trans, subvol_inum inum,
 				       hash_info, inum, &search);
 	}
 
-	if (ret == -ENOENT)
+	if (bch2_err_matches(ret, ENOENT))
 		ret = flags & XATTR_REPLACE ? -ENODATA : 0;
 
 	return ret;
